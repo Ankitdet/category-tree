d1212c90ad443b5b34e709ee69842df1d6931fe4
ankit <ankitdetroja@gmail.com>
2021-01-10 12:18:41 +0530

    init commit for Simform Assignment

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './extensions';
+import './index.css';
+import  Treeview  from './SampleTest/App';
+
+ReactDOM.render(<Treeview />, document.getElementById('root'));
+
+/* import { createMockData } from './utils';
+
+let store = createStore(reducer, createMockData(5));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+window.addEventListener('keydown', function (ev) {
+    if (window.navigator.platform.includes('Mac'))
+        window.controlPressed = ev.altKey;
+    else
+        window.controlPressed = ev.ctrlKey;
+});
+
+window.addEventListener('keyup', function (ev) {
+    if (window.navigator.platform.includes('Mac'))
+        window.controlPressed = ev.altKey;
+    else
+        window.controlPressed = ev.ctrlKey;
+});
+ */
